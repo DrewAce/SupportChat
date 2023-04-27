9e7904246fb067977affdfb79aa22ec55297598e
DrewAce <juancarmona3090@gmail.com>
2023-04-27 14:49:50 -0500

    Bot Push

---
+++ src/Home/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import SupportEngine from '../SupportEngine';
+
+import './index.css';
+
+const Home = () => {
+  return (
+    <div>
+      
+
+        <SupportEngine />
+    </div>
+  );
+}
+
+export default Home;
+
+
+
